47161095c06faac07ae8e42c18b4d3a1415ea0a8
Steven, Lee <mr938363@gmail.com>
2020-10-12 23:32:38 +0900

    Making food cards to drag and drop

---
+++ FoodParcelsDelivery/src/DragAndDropCards.js
@@ -0,0 +1,47 @@
+import * as foodCard from "./FoodCard.js";
+
+let isMouseHold = false;
+
+let selectedCardIdx;
+let offset = {};
+
+
+addEventListener("mousedown", e => {
+	for(let i = 0 ; i < foodCard.foodCards.length ; i++){
+		if(e.clientX > foodCard.foodCards[i].x && e.clientX < foodCard.foodCards[i].x + foodCard.foodCards[i].width && 
+			e.clientY > foodCard.foodCards[i].y && e.clientY < foodCard.foodCards[i].y + foodCard.foodCards[i].height){
+
+			isMouseHold = true;
+			selectedCardIdx = i;
+
+			addEventListener("mousemove", e => {
+				onMouseMove(e);
+			});
+
+			addEventListener("mouseup", e => {
+				onMouseUp(e);
+			});
+		
+			offset.x = e.clientX - notes[i].x;
+			offset.y = e.clientY - notes[i].y;
+
+			break;
+		}
+	}
+});
+
+
+function onMouseMove(e){
+	if(isMouseHold){
+		foodCard.foodCards[selectedCardIdx].x = e.clientX - offset.x;
+		[idx].y = e.clientY - offset.y;
+	}
+}
+
+function onMouseUp(e){
+	if(!isMouseHold) return;
+ 	isMouseHold = false;
+
+	removeEventListener("mousemove", onMouseMove);
+	removeEventListener("mouseup", onMouseUp);
+}
---
+++ FoodParcelsDelivery/src/FoodCards.js
@@ -0,0 +1,57 @@
+const canvas = document.getElementById("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+
+class FoodCard{
+	constructor(x, y, width, height){
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+	}
+
+	drawCard(){
+		c.fillStyle = this.color;
+		c.fillRect(this.x, this.y, this.width, this.height);
+		
+		c.lineWidth = 3;
+		c.strokeStyle = "silver";
+		c.strokeRect(this.x, this.y, this.width, this.height);
+		c.lineWidth = 1;
+		
+		c.strokeStyle = "black";
+		c.closePath();
+		
+		c.fillStyle = "black";
+	}
+	
+	setColors(color){
+		this.color = color;
+	}
+}
+
+let foodCards;
+const gapBetweenCards = 200;
+const rectWidth = canvas.width / 15;
+const rectHeight = canvas.width / 12;
+
+
+function init(){
+	foodCards = [];
+
+	let x = gapBetweenCards;
+	let y = canvas.height - 1.3 * rectHeight;
+	
+	while(x + rectWidth < canvas.width){
+		
+		foodCards.push(new FoodCard(x, y, rectWidth, rectHeight));
+
+		x = x + rectWidth + gapBetweenCards;
+	}
+}
+
+
+export { foodCards };
